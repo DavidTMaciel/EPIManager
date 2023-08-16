3900c1b60af8729a0c0d257f035995e12d514901
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-15 21:05:59 -0300

    route update epi by id

--- src/routes/epi-routes/index.js
+++ src/routes/epi-routes/index.js
@@ -1,5 +1,7 @@
 import { createNewEpi } from "../../controllers/Epis/create";
+import { updateEpiId } from "../../controllers/Epis/update";
 
 export const routesEpi = app =>{
     app.post('/create-new-epi', createNewEpi);
+    app.put('/update-epi/:id', updateEpiId);
 }
