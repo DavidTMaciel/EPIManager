3bd56c1f35bc62f0be292f197825e4a7ea5404e8
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-16 21:07:08 -0300

    route delete and update epi by id

--- src/routes/epi-routes/index.js
+++ src/routes/epi-routes/index.js
@@ -1,7 +1,11 @@
 import { createNewEpi } from "../../controllers/Epis/create";
+import { deleteEpiId } from "../../controllers/Epis/delete";
+import { listEpis } from "../../controllers/Epis/read";
 import { updateEpiId } from "../../controllers/Epis/update";
 
 export const routesEpi = app =>{
     app.post('/create-new-epi', createNewEpi);
     app.put('/update-epi/:id', updateEpiId);
+    app.get('/list-epis', listEpis);
+    app.delete('/delete-epi/:id', deleteEpiId);
 }
