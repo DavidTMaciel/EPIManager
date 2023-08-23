c6a28d7dc71aa03a7f6e36848d3579a98a91440a
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-22 21:19:05 -0300

    implement function list user by id

---
+++ src/repositorys/user/read/listUsers.js
@@ -0,0 +1,18 @@
+import { prisma } from "../../../services/prisma";
+
+
+export const listUser = async (id) =>{
+
+    const user = await prisma.user.findUnique({
+        where: {id}, 
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            password: false,
+            admin: false
+        }
+    });
+
+    return user;
+}
