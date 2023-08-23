2ff46f8577e7909c45807750ebff46a92300e3b3
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-22 21:17:23 -0300

    implement function create new user

--- src/repositorys/user/create/index.js
+++ src/repositorys/user/create/index.js
@@ -1,7 +1,17 @@
-import { prisma } from "../../../services";
-
+import { prisma } from "../../../services/prisma";
 
 export const createUser = async (data) =>{
-    const user  = await prisma.user.create(data);
 
+    const user = await prisma.user.create({
+        data,
+        select:{
+            id: true,
+            name: true,
+            email: true,
+            password: false,
+            admin: false
+        }
+    });
+
+    return user;
 }
