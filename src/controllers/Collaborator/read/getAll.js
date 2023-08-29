b05cba6eb954d2a9cc234618e3b99f654a6b1d10
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-28 21:05:48 -0300

    treating possible errors

--- src/controllers/Collaborator/read/getAll.js
+++ src/controllers/Collaborator/read/getAll.js
@@ -2,11 +2,16 @@ import { getAll } from "../../../repositorys/Collaborator/read/listAllCollaborat
 
 export const listAllCollaborators = async (request, reply) => {
     try{
+
         const collaborator = await getAll();
+        if(collaborator.length === 0){
+            reply.send({ message: "no to registered collaborators"});
+        }
+        
 
         reply.code(200).send(collaborator);
 
     }catch(err){
-        reply.code(400).send(err)
+        reply.code(400).send({err:'an internal error occurred'})
     }
 };
--- src/controllers/Collaborator/read/getUnique.js
+++ src/controllers/Collaborator/read/getUnique.js
@@ -5,10 +5,12 @@ export const listUniqueCollaborators = async (request, reply) => {
     try{
 
         const collaborator = await getUniqueCollaborator(Number(request.params.id));
+        if(collaborator.length === 0){
+            reply.send({ message: "no to registered collaborators"});
+        }
 
         reply.code(200).send(collaborator);
-
     }catch(err){
-        reply.code(400).send(err)
+        reply.code(400).send({err:'an internal error occurred'})
     }
 };
