5a437008970700ad0b8597dacc2bdc245b6f512a
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-28 21:17:04 -0300

    alterando framework de express para fastify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import express from 'express';
+import Fastify from 'fastify'
+import fastifyRoutes from './routes';
+
+
+const fastify = Fastify({
+    logger: true
+  })
+
+
+
+fastify.register(fastifyRoutes)
+
+
+fastify.get('/', function (request, reply) {
+    reply.send({ hello: 'world' })
+  })
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  console.log("Server listing address: " + address)
+})
