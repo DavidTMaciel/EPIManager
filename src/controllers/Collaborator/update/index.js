dde54b097bcafb04071e8bf8f042c4f56bc48ba3
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-28 21:06:39 -0300

    update function

--- src/controllers/Collaborator/update/index.js
+++ src/controllers/Collaborator/update/index.js
@@ -6,9 +6,15 @@ export const updateCollaboratorById = async (request, reply)=>{
         const dataUpdate = request.body
         const collaborator = await updateCollaborator(Number(request.params.id), dataUpdate);
 
-        reply.code(200).send(collaborator);
+        if(!collaborator){
+            reply.send({err: 'Collaborator no found'});
+        }
+        
 
+        reply.code(200).send(collaborator);
+        
     }catch(err){
+        console.log(err);
         reply.code(400).send(err);
     };
 }
