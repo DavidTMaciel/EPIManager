7f47f3ccb14aaed864250610ba2d3b041781bd21
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-28 21:08:27 -0300

    treating possible errors

--- src/controllers/Epis/create/index.js
+++ src/controllers/Epis/create/index.js
@@ -6,12 +6,15 @@ export const createNewEpi = async (request, reply)=>{
     try{
         await epiValidation.validate(request.body)
         
+        if(!epiValidation.validate(request.body)){
+            send('message: Failed to validate')
+        }
         const data = request.body;
         const epi = await createEpis(data);
 
         reply.code(200).send(epi);
 
     }catch(err){
-        reply.code(400).send(err);
+        reply.code(400).send({message: 'Name is required Int'});
     }
 }
