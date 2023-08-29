9f459bb4cdfdb82cdceca40322dcfd004fb05209
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-28 21:11:35 -0300

    selecting data return

--- src/repositorys/user/update/index.js
+++ src/repositorys/user/update/index.js
@@ -3,7 +3,11 @@ import { prisma } from "../../../services/prisma";
 export const updateUser = async (id, updateData) => {
     
     const user = await prisma.user.update({
-        where:{id}, data:updateData
+        where:{id}, data:updateData, select:{
+             name:true,
+             email:true,
+             admin:true
+        }
     });
 
     return user;
