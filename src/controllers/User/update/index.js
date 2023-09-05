1bf2ce7276ef30bede8e5466e486f353ef9bf1bd
DavidTMaciel <davidmacieljr122@gmail.com>
2023-09-05 16:40:22 -0300

    update validations the input

--- src/controllers/User/update/index.js
+++ src/controllers/User/update/index.js
@@ -1,16 +1,23 @@
+import { listUser } from "../../../repositorys/user/read/listUsers";
 import { updateUser } from "../../../repositorys/user/update";
 
 
-export const updateUserById = async (request, repley) =>{
+export const updateUserById = async (request, reply) =>{
 
     try{
         const updateData = request.body;
+        const userId = listUser(Number(request.params.id));
 
-        const user = await updateUser(Number(request.params.id), updateData);
-        
-        repley.code(200).send(user);
+        if (request.user.userId !== Number(request.params.id)) {
+            return reply.code(400).send({ error: "Unauthorized" });
+        }
+        else if(request.user.isAdmin == true || userId.admin == true || request.user.userId == Number(request.params.id)){
+            const user = await updateUser(Number(request.params.id), updateData);
+            reply.code(200).send(user);
+        }
+
     }catch(err){
-        repley.code(400).send(err);
+        reply.code(400).send({err:"Error updating user"});
     }
     
 
