e41181134dfddadfef8eb08db33e5d6055f22f8a
DavidTMaciel <davidmacieljr122@gmail.com>
2023-09-13 20:10:04 -0300

    improved authentication

--- src/authentication/index.js
+++ src/authentication/index.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-
+import { prisma } from "../services/prisma";
 export const authenticate = async (request, reply) => {
     try {
         const token = request.headers.authorization;
@@ -9,8 +9,16 @@ export const authenticate = async (request, reply) => {
         }
 
         const decoded = jwt.verify(token, "your-secret-key");
-
         request.user = decoded;
+
+        const user = await prisma.user.findUnique({
+            where: { id: request.user.userId },
+            select: { admin: true }
+        });
+
+        if(user){
+            request.user.isAdmin = user.admin;
+        }        
     } catch (error) {
         reply.code(401).send({ error: "Unauthorized" });
     }
