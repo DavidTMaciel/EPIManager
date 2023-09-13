9ea8a4f8c792fd036d82ed1b6b33b809dfd6c039
DavidTMaciel <davidmacieljr122@gmail.com>
2023-09-13 20:03:35 -0300

    route documentation login

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -2,7 +2,35 @@ import { newLogin } from "../../controllers/login";
 
 const routeLogin = (fastify, options, done) => {
 
-    fastify.post('/login', newLogin)
+    fastify.post('/login',{
+        schema:{
+            description:'Login',
+            tags: ['Login'],
+            body:{
+                type: 'object',
+                properties:{
+                    name: {type: 'string'},
+                    password: {type: 'string'}
+                }
+            }
+        },
+        response:{
+            200: {
+                description: 'Login successful',
+                type:'object',
+                properties:{
+                    message:{type: 'string', example: 'login success'}
+                }
+            },
+            400: {
+                description: 'Bad request',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'Invalid request data' },
+                },
+            },
+        }
+    }, newLogin)
 };
 
 export default routeLogin;
