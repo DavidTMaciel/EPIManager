9618ceb9a12d500336aca356d791f2ca67427a53
DavidTMaciel <davidmacieljr122@gmail.com>
2023-09-13 20:04:02 -0300

    route documentation user

--- src/routes/user-routes/index.js
+++ src/routes/user-routes/index.js
@@ -4,13 +4,172 @@ import { getAllUsers } from "../../controllers/User/read/getAllUser"
 import { getUser } from "../../controllers/User/read/getUser"
 import { updateUserById } from "../../controllers/User/update"
 
-export const routesUser = (fastify) =>{
+export const routesUser = (fastify) => {
 
-    
-    fastify.post('/create-new-user', createNewUser)
-    fastify.put('/update-user/:id', updateUserById)
-    fastify.delete('/delete-user/:id', deleteUserById)
-    fastify.get('/users', getAllUsers)
-    fastify.get('/user/:id', getUser)
+
+    fastify.post('/create-new-user', {
+        schema: {
+            description: 'Create a new user',
+            tags: ['User'],
+            summary: 'Create a user with name,password and email',
+            body: {
+                type: 'object',
+                properties: {
+                    name: { type: 'string' },
+                    email: { type: 'string' },
+                    password: { type: 'string' },
+                    admin: { type: 'boolean' }
+                },
+                required: ['name', 'password', 'email'],
+            }
+        },
+        response: {
+            200: {
+                description: 'User created successfully',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'User created successfully' },
+                },
+            },
+            400: {
+                description: 'Bad request',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'Invalid request data' },
+                },
+            },
+        }
+    }, createNewUser)
+    fastify.put('/update-user/:id', {
+        schema: {
+            description: 'Update a user',
+            tags: ['User'],
+            summary: 'Update a user by id',
+            params: {
+                properties: {
+                    id: {
+                        type: 'integer',
+                        description: 'User id'
+                    }
+                }
+            },
+            body: {
+                properties: {
+                    name: { type: 'string' },
+                    email: { type: 'string' },
+                    password: { type: 'string' },
+
+                },
+                required: ['name', 'password', 'email'],
+            }
+
+        },
+        response: {
+            200: {
+                description: 'User update successfully',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'User created successfully' },
+                },
+            },
+            400: {
+                description: 'Bad request',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'Invalid request data' },
+                },
+            },
+        }
+    }, updateUserById)
+    fastify.delete('/delete-user/:id', {
+        schema: {
+            description: 'Delete a user',
+            tags: ['User'],
+            summary: 'delete a user',
+            params: {
+                type: 'object',
+                properties: {
+                    id: {
+                        type: 'integer',
+                        description: 'User id'
+                    }
+                }
+            }
+        },
+        response: {
+            200: {
+                description: 'User delete successfully',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'User delete successfully' },
+                },
+            },
+            400: {
+                description: 'Bad request',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'Invalid request data' },
+                },
+            },
+        }
+
+    }, deleteUserById)
+    fastify.get('/users', {
+        schema: {
+            description: 'List all users',
+            tags: ['User'],
+            summary: 'List all users'
+        },
+        response: {
+            200: {
+                description: 'User delete successfully',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'User delete successfully' },
+                },
+            },
+            400: {
+                description: 'Bad request',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'Invalid request data' },
+                },
+            },
+        }
+
+    }, getAllUsers)
+    fastify.get('/user/:id', {
+        schema: {
+            description: 'List user by id',
+            tags: ['User'],
+            summary: 'List user',
+            params: {
+                type: 'object',
+                properties: {
+                    id: {
+                        type: 'integer',
+                        description: 'User id'
+                    }
+                }
+            }
+        },
+        response: {
+            200: {
+                description: 'User delete successfully',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'User delete successfully' },
+                },
+            },
+            400: {
+                description: 'Bad request',
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'Invalid request data' },
+                },
+            },
+        }
+    }, getUser)
+
 
 }
