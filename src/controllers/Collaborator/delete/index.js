b1d0113e855a6f6661e81f8bfde8d22cfac40dc6
DavidTMaciel <davidmacieljr122@gmail.com>
2023-09-26 22:05:41 -0300

    add validations

--- src/controllers/Collaborator/delete/index.js
+++ src/controllers/Collaborator/delete/index.js
@@ -1,11 +1,23 @@
 import { deleteCollaborator } from "../../../repositorys/Collaborator/delete";
+import { getUniqueCollaborator } from "../../../repositorys/Collaborator/read/listCollaborator";
 
 export const deleteCollaboratorById = async (request, reply) =>{
     try{
-        const collaborator = await deleteCollaborator(Number(request.params.id));
+
+        const collaboratorId = Number(request.params.id);
+
+        if(isNaN(collaboratorId) || collaboratorId <= 0){
+            reply.code(400).send({message: "Invalid collaborator"});
+        }
+        const searchCollaborator = await getUniqueCollaborator(collaboratorId)
+        if(!searchCollaborator){
+            reply.code(404).send({ message: "Collaborator not found" });
+        }
+        const collaborator = await deleteCollaborator(collaboratorId);
         
         reply.code(200).send(collaborator);
     }catch(err){
+        console.log(err)
         reply.code(400).send(err);
     };
 }
