065cba3e1adb22dd65c8caebcf985416670367e9
DavidTMaciel <davidmacieljr122@gmail.com>
2023-10-16 05:26:58 -0300

    pag home

---
+++ Front/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../../components/navbar/index";
+import Body from "../../components/body/index";
+
+function Home() {
+    return (
+        <div>
+            <NavBar />
+            <Body />
+        </div>
+    )
+}
+
+export default Home;
