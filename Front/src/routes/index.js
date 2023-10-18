28b2c4a0ce6d2e85371a574f044dc1e6deffa3cc
DavidTMaciel <davidmacieljr122@gmail.com>
2023-10-18 19:25:16 -0300

    routes front end

--- Front/src/routes/index.js
+++ Front/src/routes/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Interface from '../../src/pages/interface/index';
-import RegisterCollaborator from '../../src/components/components-interface/register/index';
+import RegisterCollaborator from '../pages/RegisterCollaborator/index'
 import Product from '../pages/Product';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import PagSignature from '../pages/PagSignature';
+import RegisterEpi from '../pages/RegisterEpi';
+import ReleaseEpi from '../pages/ReleaseEpi';
 
 function RoutesApp() {
   return (
@@ -14,9 +16,11 @@ function RoutesApp() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/interface" element={<Interface />} />
-        <Route path="/register" element={<RegisterCollaborator />} />
+        <Route path="/registerCollaborator" element={<RegisterCollaborator />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pagSignature" element={<PagSignature />} />
+        <Route path="/registerEpi" element={<RegisterEpi />} />
+        <Route path='/releaseEpi' element={<ReleaseEpi/>} />
       </Routes>
     </Router>
   );
