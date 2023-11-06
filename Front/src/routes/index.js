896afaa1e6fa374d4264f629b9e68290b358080e
DavidTMaciel <davidmacieljr122@gmail.com>
2023-11-06 18:39:31 -0300

    settings routes

--- Front/src/routes/index.js
+++ Front/src/routes/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Interface from '../../src/pages/interface/index';
-import RegisterCollaborator from '../pages/RegisterCollaborator/index'
+import RegisterCollaborator from '../pages/Collaborator/RegisterCollaborator';
 import Product from '../pages/Product';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import PagSignature from '../pages/PagSignature';
-import RegisterEpi from '../pages/RegisterEpi';
+import RegisterEpi from '../pages/Epi/RegisterEpi';
 import ReleaseEpi from '../pages/ReleaseEpi';
+import ViewCollaborator from '../pages/Collaborator/ViewCollaborator';
 
 function RoutesApp() {
   return (
@@ -16,11 +17,12 @@ function RoutesApp() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/interface" element={<Interface />} />
-        <Route path="/registerCollaborator" element={<RegisterCollaborator />} />
+        <Route path="/registerCollaborator" element={<RegisterCollaborator/>} />
         <Route path="/product" element={<Product />} />
         <Route path="/pagSignature" element={<PagSignature />} />
         <Route path="/registerEpi" element={<RegisterEpi />} />
         <Route path='/releaseEpi' element={<ReleaseEpi/>} />
+        <Route path='/collaborators' element={<ViewCollaborator />} />
       </Routes>
     </Router>
   );
