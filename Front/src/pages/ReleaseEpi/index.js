5d859c606f6b440b4b56612be70aa55a8704bcab
DavidTMaciel <davidmacieljr122@gmail.com>
2023-11-06 18:38:52 -0300

    epi release and preview screen

--- Front/src/pages/ReleaseEpi/index.js
+++ Front/src/pages/ReleaseEpi/index.js
@@ -1,28 +1,35 @@
-import React from "react";
-import './style.css';
+import React, { useState } from "react";
+import RegisterRelease from "./RegisterReleaser";
+import ViewRelease from "./ViewRelease";
 import Menu from "../../components/components-interface/menu";
-
+import './style.css'
 
 export default function ReleaseEpi() {
+
+    const [displayReleaser, setdislayRelease] = useState(true);
+
+    const toglleDisplay = () => {
+        setdislayRelease(!displayReleaser)
+    };
     return (
         <div>
             <Menu />
-            <div className="container-release">
-                <div>
-                <button>Visualizar</button>
-                <button>Registar Epi</button>
+            <div className="container-release-epi">
+                <div className="release-epi-button">
+                    <button
+                        onClick={toglleDisplay}
+                        className={displayReleaser ? 'active-button' : 'inactive-button'}
+                    >
+                        Cadastro
+                    </button>
+                    <button
+                        onClick={toglleDisplay}
+                        className={displayReleaser ? 'inactive-button' : 'active-button'}
+                    >
+                        Visualizar
+                    </button>
                 </div>
-
-                <div className="release-epi">
-                    <form method="POST">
-                        <input type="text" placeholder="Funcionario*"></input>
-                        <input type="text" placeholder="Codigo*"></input>
-                        <input type="number" placeholder="Quantidades*"></input>
-                        <input type="text" placeholder="C.A*"></input>
-                        <input type="date" placeholder="Data de Entrega*"></input>
-                        <button type="submit">Salvar</button>
-                    </form>
-                </div>
+                {displayReleaser ? <RegisterRelease /> : <ViewRelease />}
             </div>
         </div>
     )
